fd81899f100f87004c0b2b4013b83ab6ce212a03
Tony DELFORGE <tony.delforge.tgcc@cea.fr>
2017-05-12 13:36:27 +0200

    success on previous attempt

--- src/preload/gen/test_generation_wrapper.c
+++ src/preload/gen/test_generation_wrapper.c
@@ -8239,6 +8239,7 @@ in_w=1;
 
 
 int ret_tmp= LOCAL_MPI_Init( argc, argv);
+int wi4mpi_rank;
 R_MPI_Comm_rank(R_MPI_COMM_WORLD,&wi4mpi_rank);
 if(wi4mpi_rank==0)
 	fprintf(stdout,"You are using Wi4MPI-%s with the mode preload From %s To %s\n", getenv("WI4MPI_VERSION"), getenv("WI4MPI_FROM"), getenv("WI4MPI_TO"));
