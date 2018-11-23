90ed228484afb2705e6f2bf3296fff3b1f87ce13
Vincent DUCROT <ducrot@r4login.c-inti.ccc.ocre.cea.fr>
2018-11-23 16:21:14 +0100

    bugfix

--- src/common/helper.c
+++ src/common/helper.c
@@ -192,7 +192,7 @@ void timeout_config_file(void)
     char *fname=getenv("WI4MPI_TIMEOUT_CONFIG_FILE");
     if(!fname)
         return;
-    ff=fopen("fname","r");
+    ff=fopen(fname,"r");
     if(!ff)
         return;
     while(getline(&buff,&n,ff)>0){
