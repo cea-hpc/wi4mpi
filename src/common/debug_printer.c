5c6f9b0bf7d6b87371aa9b88be58f4b1bb323b23
Vincent DUCROT <ducrot@r3login.c-inti.ccc.ocre.cea.fr>
2018-11-29 17:24:42 +0100

    array support

--- src/common/debug_printer.c
+++ src/common/debug_printer.c
@@ -3,12 +3,14 @@
 #include "app_mpi.h"
 #include <stdarg.h>
 int debug_act;
+
 void debug_printer(const char *ctr_str,...)
 {
     if(debug_act!=0)
 {
     int len=strlen(ctr_str);
     char print_str[4096];
+    
     va_list ap;
     int it,i;
     va_start(ap,ctr_str);
@@ -30,16 +32,35 @@ void debug_printer(const char *ctr_str,...)
                     char cname[2048];int namelen;
                 it++;
                 to_dec=0;
+                nb_elt=1;
             switch (ctr_str[it]){
                 case '*':
                     pointer_disp++;
                     to_dec=1;    
                     break;
                 case 'n':
+                    if(nb_elt==1)
                     print_status(pointer_disp?*va_arg(ap,A_MPI_Status *):va_arg(ap,A_MPI_Status));
+                    else
+                    {
+                    A_MPI_Status *s=pointer_disp?*va_arg(ap,A_MPI_Status **):va_arg(ap,A_MPI_Status*);
+                    printf("[\n");
+                    for(ii=0;ii<nb_elt;ii++)
+                    {if(!ii) printf(",");print_status(s[ii]);}
+                    printf("]\n");
+                    }
                     break;
                 case 'd':
+                    if(nb_elt==1)
                     printf("%d",pointer_disp?*(va_arg(ap,int*)):(va_arg(ap,int)));
+                    else
+                    {
+                    int *s=pointer_disp?*va_arg(int **):va_arg(ap,int*);
+                    printf("[\n");
+                    for(ii=0;ii<nb_elt;ii++)
+                    {if(ii) printf(",\n%d",s[ii]);else printf("%d",s[ii]);}
+                    printf("]\n");
+                    }
                     break;
                 case 'D':
                      A_MPI_Datatype dat=pointer_disp?*(va_arg(ap,A_MPI_Datatype*)):(va_arg(ap,A_MPI_Datatype));
@@ -48,6 +69,10 @@ void debug_printer(const char *ctr_str,...)
                     debug_act=1;
                     printf("{ \nvalue :%p ,\n name: %s\n}",dat,cname);
                     break; 
+                case 'a':
+                    nb_elt=va_arg(ap,int);
+                    
+                    break;
                 //case 's':
                 //    printf("%s",pointer_disp?*(va_arg(ap,char **)):va_arg(ap,char*));
                 //    break;
@@ -60,7 +85,6 @@ void debug_printer(const char *ctr_str,...)
                     break; 
                default:
                     va_arg(ap,void*);
-                    it++;
                     break;
             }
             }
