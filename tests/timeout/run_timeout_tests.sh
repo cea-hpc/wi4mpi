353a96c5f97d8404893e8bd5b66f1da3a62a0c53
Adrien COTTE <adrien.cotte.tgcc@cea.fr>
2019-01-08 17:03:02 +0100

    ctest: add timeout tests (WI4MPI_MPI_Reduce_timeout)

---
+++ tests/timeout/run_timeout_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ../etc/batch.cfg || exit 1
+
+export WI4MPI_ROOT
+export WI4MPI_CC
+export WI4MPI_TO
+export LD_LIBRARY_PATH=${WI4MPI_ROOT}/lib:${LD_LIBRARY_PATH}
+export PATH=${WI4MPI_ROOT}/bin:${PATH}
+
+mpicc timeout_slow_add.c -o timeout_slow_add
+
+export WI4MPI_Reduce_timeout=1
+${WI4MPI_MPRUN} ${WI4MPI_NPROC} ${WI4MPI_NCORE} ${WI4MPI_PARTITION} ${WI4MPI_EXTRA_OPTS} ./timeout_slow_add &> timeout_slow_add.log
+
+grep -q 'processus .* on host .* has reached a timeout' timeout_slow_add.log
+
+echo $?
