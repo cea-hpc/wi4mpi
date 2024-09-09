d392cd8ff4e771ba54f77a6f4ba26809f5e5aa98
debussche <lyderic.debusschere.tgcc@cea.fr>
2024-09-09 10:15:05 +0200

    [style](lib): reach 10/10 with linter on mpichmpichheader.py

--- lib/mpichmpichheader.py
+++ lib/mpichmpichheader.py
@@ -21,11 +21,9 @@ class MpichMpichHeaderGenerator(IntelIntelHeaderGenerator):
         self,
         dir_input="src/preload/header/scripts/intel_intel_headers",
         dir_output="src/preload/header/_MPICH_MPICH_gen",
-        mpi_target_version={},
+        mpi_target_version=None,
     ):
         log.info("Generation of MPICH_MPICH headers in progress.")
         super().__init__(
-                dir_input=dir_input,
-                dir_output=dir_output,
-                mpi_target_version=mpi_target_version
+            dir_input=dir_input, dir_output=dir_output, mpi_target_version=mpi_target_version
         )
