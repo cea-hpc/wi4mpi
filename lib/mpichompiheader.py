6aaa3d1f7ee7908e03e66c29692260e611a3fcbc
debussche <lyderic.debusschere.tgcc@cea.fr>
2024-09-09 10:16:46 +0200

    [style](lib): reach 10/10 with linter on lib/mpichompiheader.py

--- lib/mpichompiheader.py
+++ lib/mpichompiheader.py
@@ -21,11 +21,9 @@ class MpichOmpiHeaderGenerator(IntelOmpiHeaderGenerator):
         self,
         dir_input="src/preload/header/scripts/intel_ompi_headers",
         dir_output="src/preload/header/_MPICH_OMPI_gen",
-        mpi_target_version={},
+        mpi_target_version=None,
     ):
         log.info("Generation of MPICH_OMPI headers in progress.")
         super().__init__(
-                dir_input=dir_input,
-                dir_output=dir_output,
-                mpi_target_version=mpi_target_version
+            dir_input=dir_input, dir_output=dir_output, mpi_target_version=mpi_target_version
         )
