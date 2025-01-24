ec0b4722b2a7345b4e110c91b253ab495a0be933
Vincent DUCROT <vincent.ducrot@cea.fr>
2025-01-24 13:37:29 +0100

    fix some codes

--- lib/etc/code/fortran/static_sources/A_f_MPI_Errhandler_get.c
+++ lib/etc/code/fortran/static_sources/A_f_MPI_Errhandler_get.c
@@ -1,25 +1,25 @@
-void A_f_MPI_Attr_get(int *comm, int *keyval, int *attribute_val, int *flag,
-                      int *ret) {
+
+void A_f_MPI_Errhandler_get(int *comm, int *errhandler, int *ret) {
 #ifdef TIMEOUT_SUPPORT
-  wi4mpi_set_timeout(WI4MPI_Attr_get_timeout);
+  wi4mpi_set_timeout(WI4MPI_Errhandler_get_timeout);
 #endif
   in_w = 1;
   int ret_tmp = 0;
 
   int comm_tmp;
-  int keyval_tmp;
+  int errhandler_tmp;
   comm_a2r(comm, &comm_tmp);
-  keyval_a2r(keyval, &keyval_tmp);
-  LOCAL_f_MPI_Attr_get(&comm_tmp, &keyval_tmp, attribute_val, flag, &ret_tmp);
+  _LOCAL_MPI_Errhandler_get(&comm_tmp, &errhandler_tmp, &ret_tmp);
+  if (ret_tmp == R_f_MPI_SUCCESS)
+    errhandler_converter_r2a(errhandler, &errhandler_tmp);
   error_r2a(ret, &ret_tmp);
 
   in_w = 0;
 #ifdef DEBUG
-  if (WI4MPI_Attr_get_print)
-    debug_printer(
-        "MPI_Attr_get : \n{\ncomm : %*C,\nkeyval : %*d,\nattribute_val : "
-        "%*d,\nflag : %*d,\nerror/return : %*d\n}\n",
-        comm, keyval, attribute_val, flag, ret);
+  if (WI4MPI_Errhandler_get_print)
+    debug_printer("MPI_Errhandler_get : \n{\ncomm : %*C,\nerrhandler : "
+                  "%*d,\nerror/return : %*d\n}\n",
+                  comm, errhandler, ret);
 #endif
 #ifdef TIMEOUT_SUPPORT
   wi4mpi_unset_timeout();
