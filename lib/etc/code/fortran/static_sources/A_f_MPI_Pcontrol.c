dc7b892664cf15c6a1a5afcf8e3aa0009018d6a5
Vincent DUCROT <vincent.ducrot@cea.fr>
2025-01-30 15:48:10 +0100

    add MPI_Pcontrol to fortran

--- lib/etc/code/fortran/static_sources/A_f_MPI_Pcontrol.c
+++ lib/etc/code/fortran/static_sources/A_f_MPI_Pcontrol.c
@@ -1,12 +1,12 @@
-void mpi_pcontrol_(int *);
+void mpi_pcontrol_(int *,int *);
 
-void mpi_pcontrol__(int *);
+void mpi_pcontrol__(int *,int *);
 
-void pmpi_pcontrol_(int *);
+void pmpi_pcontrol_(int *,int *);
 
-void pmpi_pcontrol__(int *);
+void pmpi_pcontrol__(int *,int *);
 
-void pmpi_pcontrol_(int *);
+void pmpi_pcontrol_(int *,int *);
 
 #define A_f_MPI_Pcontrol _PMPI_Pcontrol
 #pragma weak mpi_pcontrol_ = _PMPI_Pcontrol
@@ -14,16 +14,16 @@ void pmpi_pcontrol_(int *);
 #pragma weak pmpi_pcontrol__ = _PMPI_Pcontrol
 extern unsigned long long WI4MPI_Pcontrol_timeout;
 extern unsigned int WI4MPI_Pcontrol_print;
-void (*LOCAL_f_MPI_Pcontrol)(int *);
+void (*LOCAL_f_MPI_Pcontrol)(int *,int *);
 
-void A_f_MPI_Pcontrol(int *ret) {
+void A_f_MPI_Pcontrol(int *level,int *ret) {
 #ifdef TIMEOUT_SUPPORT
   wi4mpi_set_timeout(WI4MPI_Pcontrol_timeout);
 #endif
   in_w = 1;
   int ret_tmp = 0;
 
-  LOCAL_f_MPI_Pcontrol(&ret_tmp);
+  LOCAL_f_MPI_Pcontrol(level,&ret_tmp);
   error_r2a(ret, &ret_tmp);
 
 #ifdef DEBUG
